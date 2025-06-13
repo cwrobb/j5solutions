d504fa84ce0820f28f55aa2c6fbb596a6ec37708
Chris Robbins <chris@resonatems.com>
2025-06-12 19:42:45 -0500

    Initial commit

--- src/utlits/fackData/servicesDataThree.js
+++ src/utlits/fackData/servicesDataThree.js
@@ -1,6 +1,5 @@
 import service_img_1 from "../../assets/images/services/services-1.jpg"
 import service_img_2 from "../../assets/images/services/services-2.jpg"
-import service_img_3 from "../../assets/images/services/services-3.jpg"
 
 export const servicesDataThree = [
     {
